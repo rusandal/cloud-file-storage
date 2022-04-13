70198ad9c509d12aa3e0286fe8b420283cdb4d27
Sandal <rusandal@mail.com>
2022-04-13 22:54:06 +0300

    Added spring.datasource, liquibase master changelog and create table, RestController

---
+++ src/main/java/ru/minikhanov/cloud_storage/controller/StorageController.java
@@ -0,0 +1,10 @@
+package ru.minikhanov.cloud_storage.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class StorageController {
+
+}
